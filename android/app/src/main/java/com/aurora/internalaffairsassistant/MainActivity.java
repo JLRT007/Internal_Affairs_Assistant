7175c491ee79fd3348a12bc9e0bdce68a2c236e6
aurora <3479480183@qq.com>
2026-04-27 14:51:52 +0800

    Add Android APK build support

---
+++ android/app/src/main/java/com/aurora/internalaffairsassistant/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aurora.internalaffairsassistant;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aurora.internalaffairsassistant',
+  appName: 'aurora 的内务小猪手',
+  webDir: 'dist'
+};
+
+export default config;
